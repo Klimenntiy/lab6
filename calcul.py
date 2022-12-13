b258989fdb74da4b77667c5b3b1c74ea9d39a704
Klimenntiy <112969076+Klimenntiy@users.noreply.github.com>
2022-12-13 23:55:01 +0300

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,20 +1,16 @@
 def calcul (expression):
     allowed = '+-/*'
-    if not any (sign in expression for sign in allowed):
-        raise ValueError('Должен быть хотя бы один знак')
     for sign in allowed:
         if sign in expression:
-            try:
-                left, right = expression.split (sign)
-                left, right = int(left), int(right)
-                return {
-                    '+': lambda a, b: a + b,
-                    '-': lambda a, b: a - b,
-                    '*': lambda a, b: a * b,
-                    '/': lambda a, b: a / b,
+            left, right = expression.split (sign)
+            left, right = int(left), int(right)
+            return {
+                '+': lambda a, b: a + b,
+                '-': lambda a, b: a - b,
+                '*': lambda a, b: a * b,
+                '/': lambda a, b: a / b,
                 }[sign](left, right)
-            except (ValueError, TypeError):
-                raise ValueError ('2 целых числа')
+            
 
 if __name__ == '__main__':
     calcul('')
