3712e2b933d58eb24e2b75b5346fc5d611a067ad
Klimenntiy <112969076+Klimenntiy@users.noreply.github.com>
2022-12-14 11:31:36 +0300

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,19 +1,19 @@
-def calculate (operation):
-    operation = list(operation.split())
-    a = int (operation[0])
-    b = int (operation[2])
-    operator = operation[1]
-    answer = "wrong input";
-    if (operator == '+'):
-        answer = a + b
-    elif (operator == '-'):
-        answer = a - b
-    elif (operator == '*'):
-        answer a * b
-    elif (operator == '/'):
-        answer = a / b
-    return answer
-
-if __name__ == "__main__":
-    operation = input()
-    print (calculate(operation))
+def calcul(expression):
+    allowed = '+-/*'
+    for sign in allowed:
+        if sign in expression:
+            try:
+                a, b = expression.split(sign)
+                a, b = int(a), int(b)
+                if sign == '+':
+                    return a + b
+                if sign == '-':
+                    return a - b
+                if sign == '*':
+                    return a * b
+                if sign == '/':
+                    return a / b
+            except(ValueError, TypeError):
+                raise ValueError('не корректный ввод')
+if __name__ == '__main__':
+    calcul('')
